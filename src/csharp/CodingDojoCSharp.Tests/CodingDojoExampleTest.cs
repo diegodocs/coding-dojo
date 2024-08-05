c371e604278dd1b64e4f286547e9564a6b395138
Diego Cardoso <DiegoCardoso@microsoft.com>
2024-08-05 12:10:02 -0300

    update instructions

--- src/csharp/CodingDojoCSharp.Tests/CodingDojoExampleTest.cs
+++ src/csharp/CodingDojoCSharp.Tests/CodingDojoExampleTest.cs
@@ -4,10 +4,25 @@ namespace CodingDojoCSharp.Tests
     {
         
         [Test]
-        public void When_Conditions_Then_Results()
+        public void When_TestConditions_Then_Results()
         {
             //arrange
             var expectedValue = true;
+            var currentValue = false;
+            var dojo = new CodingDojoExample();
+
+            //act
+            currentValue = dojo.Calculate();
+
+            //assert
+            Assert.That(currentValue, Is.EqualTo(expectedValue));
+
+        }
+
+        [TestCase(true)]
+        public void When_TestCaseConditions_Then_Results(bool expectedValue)
+        {
+            //arrange
             var currentValue = false;
             var dojo = new CodingDojoExample();
 
